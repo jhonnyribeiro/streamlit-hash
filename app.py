67a07a83d9c461c6e43482db44b478e65db05ce3
Jhonny Ribeiro <jaribeiro32@gmail.com>
2025-03-13 20:16:57 -0300

    select tickers

--- app.py
+++ app.py
@@ -4,19 +4,30 @@ import yfinance as yf
 
 #load data
 @st.cache_data
-def carregar_dados(empresa):
-    dados_acao = yf.Ticker(empresa)
+def carregar_dados(empresas):
+    texto_tickers = " ".join(empresas)
+    dados_acao = yf.Tickers(texto_tickers)
     cotacoes_acao = dados_acao.history(period="1d", start='2010-01-01', end='2024-07-01')
-    cotacoes_acao = cotacoes_acao[['Close']]
+    print(cotacoes_acao)
+    cotacoes_acao = cotacoes_acao['Close']
     return cotacoes_acao
 
-#prepare vizualization
-dados = carregar_dados("ITUB4.SA")
-    
+acoes = ['ITSA4.SA', 'PETR4.SA', 'MGLU3.SA', 'VALE3.SA', 'ABEV3.SA', 'GGBR4.SA']
+dados = carregar_dados(acoes)
 
 st.write("""
 # App Preço de Ações
 O gráfico abaixo representa o preço das ações do xxxx (Tiker) ao longo dos anos.
          """)
+#prepare vizualization
+
+lista_acoes = st.multiselect("Ações",dados.columns)
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes) == 1:
+        acao_unica = lista_acoes[0]
+        dados = dados.rename(columns={acao_unica: "Close"})
+ 
+
 
 st.line_chart(dados)
